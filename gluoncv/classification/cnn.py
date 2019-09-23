361c167eac48463c074dd39356924033a7e2ca68
Joshua Z. Zhang <cheungchih@gmail.com>
2019-09-23 15:05:48 -0700

    update model filtering

--- gluoncv/classification/cnn.py
+++ gluoncv/classification/cnn.py
@@ -16,8 +16,10 @@ def _preprocess(X):
     X = nd.array(X).transpose((0,3,1,2))
     return (X.astype('float32') / 255 - rgb_mean) / rgb_std
 
-blacklist = ['faster', 'ssd', 'yolo3', 'fcn', 'psp', 'mask', 'cifar', 'deeplab']
+blacklist = ['faster', 'ssd', 'yolo3', 'fcn', 'psp', 'mask', 'cifar', 'deeplab', 'simple_pose', 'alpha_pose']
 model_list = [x for x in gcv.model_zoo.pretrained_model_list() if x.split('_')[0].lower() not in blacklist]
+action_list = ['kinetics400', 'ucf101']
+model_list = [x for x in model_list if x.split('_')[1].lower() not in action_list]
 
 def get_accuracy(model_name):
     batch_size = 64
